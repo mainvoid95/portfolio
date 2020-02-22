70e967e84dfae430d68c24a41f0f99d37f5f233f
정종헌 <mainvoid95@gmail.com>
2020-02-22 17:01:36 +0900

    profile 컴포넌트 추가 (아직 덜만듬)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Title from './components/title';
 import Footer from './components/footer';
 import Header from './components/header';
 import Profile from './components/profile';
+import Projects from './components/projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header></Header>
       <Title></Title>
       <Profile></Profile>
+      <Projects></Projects>
       <Footer></Footer>
     </div>
   );
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 import React from "react"
-import Github from '../img/GitHub.png';
+import Github from '../img/github.png';
 
 export default function footer(){
     return(
         <footer>
-            <a href="https://github.com/mainvoid95"><img src={Github}></img></a>
+            <a href="https://github.com/mainvoid95"><img className="github" src={Github}></img></a>
         </footer>
     )
 }
